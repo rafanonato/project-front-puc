2bcfda58e8f66e025aa2c9a40e0b09ad14b7bf84
nonato <nonato@mac-nonato.local>
2019-09-06 15:18:58 -0300

    add component cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    pathMatch:'full',
+    component: AppComponent
+  },
+  {
+    path:'cadastro',
+    component: CadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
