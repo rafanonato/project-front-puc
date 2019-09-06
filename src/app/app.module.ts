bf3054b6d6cb6120b1fdac896ad2bec26d7ff336
nonato <nonato@mac-nonato.local>
2019-09-06 17:59:18 -0300

    correct bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { AcessarComponent } from './acessar/acessar.component';
 import { LogadoComponent } from './logado/logado.component';
 import { BarragemComponent } from './barragem/barragem.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { BarragemComponent } from './barragem/barragem.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent, CadastroComponent, AcessarComponent, LogadoComponent, BarragemComponent]
--- src/app/barragem/barragem.component.ts
+++ src/app/barragem/barragem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-barragem',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarragemComponent implements OnInit {
 
-  constructor() { }
+  baseUrl: 'https://24994e42-e007-43b6-b9f6-eb2e92e23c9f.mock.pstmn.io';
+  private barragens  = []; 
+
+  constructor(private httpClient: HttpClient){}
 
   ngOnInit() {
   }
+
+
+
+
+get_baragens(){
+    this.httpClient.get(this.baseUrl + '/barragem/cadastrar').subscribe((res : any[])=>{
+    console.log(res);
+    this.barragens = res;
+    });
+  }
+
+myFunc(){
+    console.log("function called");
+  }
+
 
 }
